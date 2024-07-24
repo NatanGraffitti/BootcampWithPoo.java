48fad7f876c72d35906a675b31403b36ceaeb6a1
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2024-07-24 16:51:34 -0300

    Create Bootcamp.java

---
+++ Bootcamp.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Bootcamp {
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new HashSet<>();
+
+    // Getters e Setters
+
+    @Override
+    public String toString() {
+        return "Bootcamp{" +
+                "nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataInicial=" + dataInicial +
+                ", dataFinal=" + dataFinal +
+                ", devsInscritos=" + devsInscritos +
+                ", conteudos=" + conteudos +
+                '}';
+    }
+}
